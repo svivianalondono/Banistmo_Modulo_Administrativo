11946c17faf5fe810c6c1506f6db4fa1e90a7d85
Meneses <cristian.c.meneses@accenture.com>
2018-01-03 14:43:05 -0500

    componente menu CNB

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
+
 import { HomeAdministrativoComponent } from './components/home-administrativo/home-administrativo.component';
-=======
 import { AdministrativeLoginComponent } from './components/administrative-login/administrative-login.component';
->>>>>>> 9fe9a488b212833788f43a0409c2871ed779e230
+import { MenuCnbComponent } from './components/menu-cnb/menu-cnb.component';
+
 
 const routes: Routes = [
   { path: 'Login', component: AdministrativeLoginComponent },
+  { path: 'Home', component: HomeAdministrativoComponent },  
+  { path: 'CNBMenu', component: MenuCnbComponent },  
   { path: '**', redirectTo: 'Login', pathMatch: 'full' }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    HomeAdministrativoComponent
-=======
-    AdministrativeLoginComponent
->>>>>>> 9fe9a488b212833788f43a0409c2871ed779e230
+    HomeAdministrativoComponent,
+    AdministrativeLoginComponent,
+    MenuCnbComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/menu-cnb/menu-cnb.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-cnb',
+  templateUrl: './menu-cnb.component.html',
+  styleUrls: ['./menu-cnb.component.scss']
+})
+export class MenuCnbComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
