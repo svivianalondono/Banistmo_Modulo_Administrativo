9fe9a488b212833788f43a0409c2871ed779e230
DIR\sandra.londono <sandra.londono@accenture.com>
2018-01-03 14:24:09 -0500

    Adicionando bootstrap al prroyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AdministrativeLoginComponent } from './components/administrative-login/administrative-login.component';
 
+const routes: Routes = [
+  { path: 'Login', component: AdministrativeLoginComponent },
+  { path: '**', redirectTo: 'Login', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdministrativeLoginComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/administrative-login/administrative-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-administrative-login',
+  templateUrl: './administrative-login.component.html',
+  styleUrls: ['./administrative-login.component.scss']
+})
+export class AdministrativeLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
