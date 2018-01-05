87abce2746bfa6df2f868ccafd0dbc4a509eea81
Meneses <cristian.c.meneses@accenture.com>
2018-01-05 17:57:53 -0500

    modificacion de home

--- src/app/components/home-administrativo/home-administrativo.component.ts
+++ src/app/components/home-administrativo/home-administrativo.component.ts
@@ -7,17 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeAdministrativoComponent implements OnInit {
   public menuCNB : boolean = false;
+  public home : boolean = true;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  mostrarMainCNB(){
-    if(this.menuCNB)
-      this.menuCNB = false;
-    else
-      this.menuCNB = true;
+  cerrarDiv(){
+    this.home = false;
+    this.menuCNB = false;
+  }
+
+  mostrarMain(key){
+    // alert("entro a mostrar");
+    this.cerrarDiv();
+    switch (key) {
+      
+      case 'home':
+      // alert("entro a home");
+        this.home = true;
+        break;
+      case 'CNB':        
+        this.menuCNB = true;        
+        break;
+    
+      default:
+        break;
+    }
+   
   }
 
 }
