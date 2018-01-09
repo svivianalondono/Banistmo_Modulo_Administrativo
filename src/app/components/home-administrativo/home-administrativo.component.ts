4e0d165ceed3cbb32aab3bc027882c2d260688bf
Meneses <cristian.c.meneses@accenture.com>
2018-01-08 19:35:10 -0500

    modificacion home componente

--- src/app/components/home-administrativo/home-administrativo.component.ts
+++ src/app/components/home-administrativo/home-administrativo.component.ts
@@ -6,8 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-administrativo.component.scss']
 })
 export class HomeAdministrativoComponent implements OnInit {
-  public menuCNB : boolean = false;
-  public home : boolean = true;
+  public menuCNB : boolean = false; //valida que las opciones del menu navbar del canal CNB se muestr o no.
+  public home : boolean = true; //valida que las opciones del home se muestre o no.
+  public switMenu = [false,false];//valida que los 2 menus no se solapen. 
+  
+  // variable temporal para separar
+  Separar= ["","","","","","",""]
 
   constructor() { }
 
@@ -36,6 +40,34 @@ export class HomeAdministrativoComponent implements OnInit {
         break;
     }
    
+  }
+
+  ocultarMenu(value){
+    // validamos quien esta hacieno la peticion
+    if (value) {
+      // validamos si el elemento esta activo
+      if (this.switMenu[0]) {
+        this.switMenu[0] = false;
+      } else {
+        if (this.switMenu[1]) {
+          document.getElementById("secondMenuToggle").click();
+          this.switMenu[1] = false;          
+        }
+        this.switMenu[0] = true;
+      }   
+      // console.log("navar: " + this.switMenu[1] +" y swit: "+ this.switMenu[0] )
+    } else {
+      if (this.switMenu[1]) {
+        this.switMenu[1] = false;
+      } else {
+        if (this.switMenu[0]) {
+          document.getElementById("menu-switch").click();
+          this.switMenu[0] = false;
+        }
+        this.switMenu[1] = true;
+      }
+      // console.log("navar: " + this.switMenu[1] +" y swit: "+ this.switMenu[0] )
+    } 
   }
 
 }
