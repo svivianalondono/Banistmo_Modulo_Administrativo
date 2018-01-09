3072eda5215cd077fec40211651ece62fa65ac61
DIR\sandra.londono <sandra.londono@accenture.com>
2018-01-09 09:05:11 -0500

    Adición de comentarios en el interceptor

--- src/app/providers/interceptor-provider/interceptor-provider.service.ts
+++ src/app/providers/interceptor-provider/interceptor-provider.service.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs/Observable';
 
 /**
  * Interceptor. Utilizado para interceptar las peticiones de salida
- * En éste se envía el autorization requerido.
+ * En éste se envía la información que va a ir en el header (el authorization requerido).
  */
 
 @Injectable()
@@ -27,11 +27,11 @@ export class InterceptorProviderService implements HttpInterceptor {
    */
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let auth; //Variable que contendrá lo que se va a enviar al authorization
-    if (localStorage.getItem("access_token") == undefined)//Se verifica el token que exista en el localStorage
-      auth = "Basic YmFuaXN0bW9BVE06Y25ic2VjcmV0QVRN";//Authorization requerido para acceder al microservicio de login
+    if (localStorage.getItem("access_token") == undefined)//Se verifica que el token exista en el localStorage
+      auth = "Basic YmFuaXN0bW9BVE06Y25ic2VjcmV0QVRN";//Authorization requerido para acceder al microservicio de login. Por configuración de ws es éste por defecto.
     else
       auth = "bearer " + localStorage.getItem("access_token");
-    request = request.clone({
+    request = request.clone({ //Para sobreescribir el request
       setHeaders: {//Adicionamos los headers necesarios
         "Authorization": auth,
         "Content-Type": "application/json"
