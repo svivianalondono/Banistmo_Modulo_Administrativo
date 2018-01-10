f1a0b10e1ada19b09c96d2bf39c98fa9df636e0a
DIR\sandra.londono <sandra.londono@accenture.com>
2018-01-10 16:56:40 -0500

    Pruebas unitarias login

--- src/app/components/administrative-login/administrative-login.component.spec.ts
+++ src/app/components/administrative-login/administrative-login.component.spec.ts
@@ -1,14 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
 import { AdministrativeLoginComponent } from './administrative-login.component';
+import { AdministrativeErrorComponent } from '../administrative-error/administrative-error.component';
+import { Router, ActivatedRoute } from '@angular/router';
+import { LoginProviderService } from '../../providers/login-provider/login-provider.service';
+import { HttpClient, HttpHandler, } from '@angular/common/http';
 
-describe('AdministrativeLoginComponent', () => {
+describe('AdministrativeLoginComponentTest', () => {
   let component: AdministrativeLoginComponent;
   let fixture: ComponentFixture<AdministrativeLoginComponent>;
 
+  /**
+   * ???
+   */
+  const fakeActivatedRoute = {
+    snapshot: { data: {} }
+  } as ActivatedRoute;
+
+  //???
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdministrativeLoginComponent ]
+      imports: [ FormsModule ],
+      declarations: [ AdministrativeLoginComponent, AdministrativeErrorComponent ],
+      providers:[ {provide: ActivatedRoute, useValue: fakeActivatedRoute},
+        { 
+          provide: Router, 
+          useClass: class { navigate = jasmine.createSpy("navigate");}
+        }, LoginProviderService, HttpClient, HttpHandler ]
     })
     .compileComponents();
   }));
@@ -21,5 +39,26 @@ describe('AdministrativeLoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  /**
+   * Test para probar la función isEmpty cuando el string es vacío
+   */
+  it('should validate a empty string', () => {
+    expect(component.isEmpty("")).toBeTruthy();
+  });
+
+  /**
+   * Test para probar la función isEmpty cuando el string es indefinido
+   */
+  it('should validate a undefined value', () => {
+    expect(component.isEmpty(undefined)).toBeTruthy();
+  });
+
+  /**
+   * Test para probar la función isEmpty cuando el string contiene caracteres
+   */
+  it('should validate a correct username', () => {
+    expect(component.isEmpty("diego")).toBeFalsy();
   });
 });
